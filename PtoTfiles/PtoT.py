d4190f25b9099bf07e244ac6fa6b98c15b4fdc41
harunaMIT <hattan.nattsun@softbank.ne.jp>
2021-04-25 18:12:49 +0900

    PtoT.py完了

--- PtoTfiles/PtoT.py
+++ PtoTfiles/PtoT.py
@@ -36,47 +36,47 @@ for file in files:
 
     return []  # その他の場合は空リストを返す。
 
-# Layout Analysisのパラメーターを設定。縦書きの検出を有効にする。
-laparams = LAParams(detect_vertical=True)
+  # Layout Analysisのパラメーターを設定。縦書きの検出を有効にする。
+  laparams = LAParams(detect_vertical=True)
 
-# 共有のリソースを管理するリソースマネージャーを作成。
-resource_manager = PDFResourceManager()
+  # 共有のリソースを管理するリソースマネージャーを作成。
+  resource_manager = PDFResourceManager()
 
-# ページを集めるPageAggregatorオブジェクトを作成。
-device = PDFPageAggregator(resource_manager, laparams=laparams)
+  # ページを集めるPageAggregatorオブジェクトを作成。
+  device = PDFPageAggregator(resource_manager, laparams=laparams)
 
-# Interpreterオブジェクトを作成。
-interpreter = PDFPageInterpreter(resource_manager, device)
+  # Interpreterオブジェクトを作成。
+  interpreter = PDFPageInterpreter(resource_manager, device)
 
-# 出力用のテキストファイル
-output_txt = open(file + '.txt', 'w')
+  # 出力用のテキストファイル
+  output_txt = open(file + '.txt', 'w')
 
-def print_and_write(txt):
-    print(txt)
-    output_txt.write(txt)
-    output_txt.write('\n')
+  def print_and_write(txt):
+      print(txt)
+      output_txt.write(txt)
+      output_txt.write('\n')
 
-with open(file, 'rb') as f:
-    
-    # PDFPage.get_pages()にファイルオブジェクトを指定して、PDFPageオブジェクトを順に取得する。
-    # 時間がかかるファイルは、キーワード引数pagenosで処理するページ番号（0始まり）のリストを指定するとよい。
-    for page in PDFPage.get_pages(f):
-        print_and_write('\n====== ページ区切り ======\n')
-        interpreter.process_page(page)  # ページを処理する。
-        layout = device.get_result()  # LTPageオブジェクトを取得。
+  with open(file, 'rb') as f:
+      
+      # PDFPage.get_pages()にファイルオブジェクトを指定して、PDFPageオブジェクトを順に取得する。
+      # 時間がかかるファイルは、キーワード引数pagenosで処理するページ番号（0始まり）のリストを指定するとよい。
+      for page in PDFPage.get_pages(f):
+          print_and_write('\n====== ページ区切り ======\n')
+          interpreter.process_page(page)  # ページを処理する。
+          layout = device.get_result()  # LTPageオブジェクトを取得。
 
-        # ページ内のテキストボックスのリストを取得する。
-        boxes = find_textboxes_recursively(layout)
+          # ページ内のテキストボックスのリストを取得する。
+          boxes = find_textboxes_recursively(layout)
 
-        # テキストボックスの左上の座標の順でテキストボックスをソートする。
-        # y1（Y座標の値）は上に行くほど大きくなるので、正負を反転させている。
-        boxes.sort(key=lambda b: (-b.y1, b.x0))
+          # テキストボックスの左上の座標の順でテキストボックスをソートする。
+          # y1（Y座標の値）は上に行くほど大きくなるので、正負を反転させている。
+          boxes.sort(key=lambda b: (-b.y1, b.x0))
 
-        for box in boxes:
-            print_and_write('-' * 10)  # 読みやすいよう区切り線を表示する。
-            print_and_write(box.get_text().strip())  # テキストボックス内のテキストを表示する。
+          for box in boxes:
+              print_and_write('-' * 10)  # 読みやすいよう区切り線を表示する。
+              print_and_write(box.get_text().strip())  # テキストボックス内のテキストを表示する。
 
-output_txt.close()
+  output_txt.close()
 
     # # 行う処理 今回はシンプルに内容の表示
     # print(f.read())
