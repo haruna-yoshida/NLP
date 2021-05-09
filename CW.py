05bf82397e5855655d10787f187db0fd4ae069ef
harunaMIT <hattan.nattsun@softbank.ne.jp>
2021-05-09 18:05:13 +0900

    グラフのpng保存

--- CW.py
+++ CW.py
@@ -1,4 +1,5 @@
 import MeCab
+import glob
 import collections
 import numpy as np
 import matplotlib as mpl
@@ -6,37 +7,45 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-#問題の取り込み
-f = open('/Users/haruna/Desktop/洗足算数/2020年/第1回/202011.txt')
-text = f.read()  # ファイル終端まで全て読んだデータを返す
-f.close()
+# filesに指定の階層のファイルを全て取得
+files = glob.glob('**/*.pdf.txt', recursive=True)
 
-print(mpl.rcParams['font.family'])
+for file in files:
 
+    #問題の取り込み
+    f = open(file)
+    text = f.read()  # ファイル終端まで全て読んだデータを返す
+    f.close()
 
-#MeCabで分割
-m = MeCab.Tagger ('-Ochasen')
- 
-node = m.parseToNode(text)
-words=[]
-while node:
-    hinshi = node.feature.split(",")[0]
-    if hinshi in ["動詞"]:
-        origin = node.feature.split(",")[6]
-        words.append(origin)
-    node = node.next
+    print(mpl.rcParams['font.family'])
 
-#単語の数カウント
-c = collections.Counter(words)
-print(c.most_common(20))
+    #MeCabで分割
+    m = MeCab.Tagger ('-Ochasen')
+    
+    node = m.parseToNode(text)
+    words=[]
+    while node:
+        hinshi = node.feature.split(",")[0]
+        if hinshi in ["動詞"]:
+            origin = node.feature.split(",")[6]
+            words.append(origin)
+        node = node.next
 
-#グラフ化
-sns.set(context="talk")
-sns.set(font='Hiragino sans')
-fig = plt.subplots(figsize=(8, 8))
- 
-sns.countplot(y=words,order=[i[0] for i in c.most_common(20)])
+    #単語の数カウント
+    c = collections.Counter(words)
+    print(c.most_common(20))
 
-print(words)
-plt.show()
+    #グラフ化
+    sns.set(context="talk")
+    sns.set(font='Hiragino sans')
+    fig = plt.subplots(figsize=(8, 8))
+    
+    sns.countplot(y=words,order=[i[0] for i in c.most_common(20)])
+
+    line_plot = sns.lineplot()
+    figure = line_plot.get_figure()
+    figure.savefig(file + '.png')
+
+    print(words)
+    plt.show()
 
